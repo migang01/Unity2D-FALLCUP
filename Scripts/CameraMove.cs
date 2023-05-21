2ea38de3a07fafbd40c9fa128cc1e71f4ebfaf6b
Migang Jung <130452988+migang01@users.noreply.github.com>
2023-05-21 21:48:14 +0900

    Update CameraMove.cs

--- Scripts/CameraMove.cs
+++ Scripts/CameraMove.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
     public Transform target;
-    public float speed;
 
     void LateUpdate()
     {
